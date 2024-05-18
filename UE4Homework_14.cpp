c0d1b5ded5bfb6ea7fe02e802581eb6df674ba96
MrZorg111 <yodji333@mail.ru>
2024-05-18 22:07:57 +0300

    Добавьте файлы проекта.

---
+++ UE4Homework_14.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <string>
+
+int main() {
+    std::string text;
+   
+
+    std::cout << "Input text:" << std::endl;
+    std::cin >> text;
+
+    std::cout << "Line length " << text.length() << std::endl;
+    std::cout << "The first character: " << text[0] << std::endl;
+    std::cout << "The last character: " << text[text.length() - 1] << std::endl;
+}
